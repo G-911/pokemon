a42f9dd765c0903cd268099c6f7293560bb13804
G-911 <g.avila-2002@hotmail.com>
2024-04-27 11:49:16 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import getPokemonOption from './assets/api/getPokemonOptions';
+
+getPokemonOption()
+
+</script>
+
+<template>
+  <p class="text-9xl"> hola</p>
+</template>
---
+++ src/assets/api/getPokemonOptions.ts
@@ -0,0 +1,16 @@
+import { pokemonApi } from "./pokemon";
+
+import type { pokemon } from "env";
+
+const getPokemons = ():any[] =>  {
+    const pokemonArr = Array.from(Array(650))
+    return pokemonArr.map((_, index) => index+1)
+}
+
+
+const getPokemonOption =()=> {
+    const pokemons = getPokemons()
+    console.log(pokemons)
+}
+
+export default getPokemonOption
