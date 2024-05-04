f57388c50c19727adc6e3e1c0899e1e4c45af41c
G-911 <g.avila-2002@hotmail.com>
2024-05-04 14:38:50 -0400

    pokemon options

--- src/assets/api/getPokemonOptions.ts
+++ src/assets/api/getPokemonOptions.ts
@@ -2,15 +2,45 @@ import { pokemonApi } from "./pokemon";
 
 import type { pokemon } from "env";
 
-const getPokemons = ():any[] =>  {
+const getPokemons = ():number[] =>  {
     const pokemonArr = Array.from(Array(650))
     return pokemonArr.map((_, index) => index+1)
 }
 
 
-const getPokemonOption =()=> {
-    const pokemons = getPokemons()
+const pokemonName = async(pokemons: number[]): Promise<pokemon[]> => {
+    if(pokemons.length != 4) throw "Error"
+    
+    const [a,b,c,d] = pokemons
+
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`)
+    ]
+
+    const [{data : p1}, {data : p2}, {data : p3}, {data : p4}] = await Promise.all(promiseArr)
+
+    return[
+        {name : p1.name, id : p1.id},
+        {name : p2.name, id : p2.id},
+        {name : p3.name, id : p3.id},
+        {name : p4.name, id : p4.id}
+    ]
+}
+
+
+const getPokemonOption = async ()=> {
+    const mixedPokemons = getPokemons().sort(()=>Math.random() -0.5)
+    
+    console.log(mixedPokemons)
+
+    const pokemons = await pokemonName(mixedPokemons.splice(0.4))
+    
     console.log(pokemons)
+
+    return pokemons
 }
 
 export default getPokemonOption
