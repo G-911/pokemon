c3cbb766b7fe99d119474dd3efa3c30df03f8ed6
G-911 <g.avila-2002@hotmail.com>
2024-05-25 11:11:57 -0400

    actualizado2

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,6 @@
 /// <reference types="vite/client" />
 
-<<<<<<< HEAD
 export interface Pokemon{
     id: number
     name: string
-=======
-export interface pokemon{
-    id : number
-    name : string
->>>>>>> f57388c50c19727adc6e3e1c0899e1e4c45af41c
 }
