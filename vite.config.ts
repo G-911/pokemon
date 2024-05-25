51ee9ab0700993afad3c6927a0dfc370fdb77716
G-911 <g.avila-2002@hotmail.com>
2024-05-25 11:07:57 -0400

    actualizado2

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-<<<<<<< HEAD
 import vueJsx from '@vitejs/plugin-vue-jsx'
-=======
->>>>>>> f57388c50c19727adc6e3e1c0899e1e4c45af41c
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,10 +11,7 @@ export default defineConfig({
 <<<<<<< HEAD
     vueJsx(),
 =======
->>>>>>> f57388c50c19727adc6e3e1c0899e1e4c45af41c
   ],
-  resolve: {
-    alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
